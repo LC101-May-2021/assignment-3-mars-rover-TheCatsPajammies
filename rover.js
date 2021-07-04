5622bf88a375a6b91887189f3defec4d2c6c2d66
Paul Thompson <67980898+TheCatsPajammies@users.noreply.github.com>
2021-07-04 23:16:53 +0000

    Cleaning up code for readability in the rover.js file.

--- rover.js
+++ rover.js
@@ -1,5 +1,5 @@
-const Command = require('./command.js');
-const Message = require('./message.js');
+//const Command = require('./command.js');
+//const Message = require('./message.js');
 
 class Rover {
   // Write code here!
@@ -10,15 +10,24 @@ class Rover {
   }
   receiveMessage(message) {
     
-    let roverStatusObj = { completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position} }
     let resultsArr = [];
+    let data = { message: message.name, results: resultsArr };
     let passedCommand = { completed: true };
     let failedCommand = { completed: false };
+    let roverStatusObj = { 
+      completed: true, 
+      roverStatus: {
+        mode: this.mode, 
+        generatorWatts: this.generatorWatts, 
+        position: this.position
+      } 
+    };
 
     for (let i = 0; i < message.commands.length; i++) {
       
       let roverCommands = message.commands[i].commandType;
       let roverCommandValue = message.commands[i].value;
+      
       if (roverCommands === 'MODE_CHANGE') {
         roverStatusObj.roverStatus.mode = roverCommandValue;
         this.mode = roverStatusObj.roverStatus.mode;
@@ -33,10 +42,6 @@ class Rover {
         resultsArr.push(roverStatusObj);
       }
     }
-
-    let data = { message: message.name, results: resultsArr }
-    
-
     return data;
   }
 
